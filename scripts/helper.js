7c6bedf94619a2caae98f21fc653ebabf95c8ef0
Kara Taylor <kararenfrotaylor@gmail.com>
2018-10-26 16:02:26 -0400

    third-attempt

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,9 @@
 class Helper {
-  playPauseAndUpdate (song = player.playPause){
-    
+  playPauseAndUpdate (song) {
+    player.playPause('song');
   }
 }
+
+$('#time-control .total-time').text()
 
 const helper = new Helper();
