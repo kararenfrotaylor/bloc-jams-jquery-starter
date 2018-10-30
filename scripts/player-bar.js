7ace6d2d56c15b046282e5e20fdc4eb5ea359d6b
Kara R. Taylor <kararenfrotaylor@gmail.com>
2018-10-30 12:36:41 -0400

    sixth attempt

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,20 +1,18 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
   $('button#next').on('click', function () {
     if (player.playState !== 'playing') {return;}
 
-    $('#time-control .total-time').text(player.currentlyPlaying.duration);
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) {return;}
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('#time-control input').on('input', function (event) {
@@ -37,14 +35,12 @@
   $('button#previous').on('click', function (){
     if (player.playState !== 'playing') {return;}
 
-    $('#time-control .total-time').text(player.currentlyPlaying.duration);
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
     if (previousSongIndex === -1) {return;}
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
 }
